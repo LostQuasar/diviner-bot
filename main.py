750a257fa184ed681352af6942a89499b5787dff
LostQuasar <github@lostquasar.dev>
2026-05-09 20:11:07 +0000

    Update discord.py

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
 import discord
+from discord import app_commands
 import dotenv
 import os
 import pubchem
-
-bot = discord.Bot()
 dotenv.load_dotenv()
 TOKEN = os.getenv("TOKEN")
+GUILD_ID = os.getenv("GUILD_ID")
 
-@bot.command(description="Get information about a compound")
-async def compound(ctx, name):
-    compound_info = pubchem.compound(name)
+if not GUILD_ID:
+    exit()
+
+MY_GUILD = discord.Object(id=int(GUILD_ID))
+
+class MyClient(discord.Client):
+    # Suppress error on the User attribute being None since it fills up later
+    user: discord.ClientUser
+
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        # A CommandTree is a special type that holds all the application command
+        # state required to make it work. This is a separate class because it
+        # allows all the extra state to be opt-in.
+        # Whenever you want to work with application commands, your tree is used
+        # to store and work with them.
+        # Note: When using commands.Bot instead of discord.Client, the bot will
+        # maintain its own tree instead.
+        self.tree = app_commands.CommandTree(self)
+
+    # In this basic example, we just synchronize the app commands to one guild.
+    # Instead of specifying a guild to every command, we copy over our global commands instead.
+    # By doing so, we don't have to wait up to an hour until they are shown to the end-user.
+    async def setup_hook(self):
+        # This copies the global commands over to your guild.
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
+
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user} (ID: {client.user.id})')
+    print('------')
+
+
+@client.tree.command()
+async def compound(interaction: discord.Interaction, name: str):
+    """Get information about a compound!"""
+    compound_info = pubchem.compound()
+    await compound_info.create(name)
     if compound_info.cid == 0:
-        await ctx.respond(f"Your search did not return any results", ephemeral=True)
+        await interaction.response.send_message(f"Your search did not return any results", ephemeral=True)
         return
 
     embed = discord.Embed(
@@ -19,7 +58,7 @@ async def compound(ctx, name):
         url=compound_info.url,
         color=discord.Colour.random(seed=compound_info.cid)
     )
-    
+
     embed.add_field(name="Molecular Formula", value=compound_info.molecular_formula, inline=True)
     embed.add_field(name="Molecular Weight", value=f"{compound_info.molecular_weight} mol", inline=True)
     if compound_info.description != "":
@@ -28,6 +67,7 @@ async def compound(ctx, name):
     embed.set_footer(text="Data provided by NIH PubChem Database", icon_url="https://pubchem.ncbi.nlm.nih.gov/pcfe/favicon/favicon-32x32.png")
     embed.set_image(url=compound_info.image_url)
 
-    await ctx.respond(embed=embed)
+    await interaction.response.send_message("",embed=embed)
 
-bot.run(TOKEN)
+client.run(TOKEN)
+
--- pubchem.py
+++ pubchem.py
@@ -1,4 +1,6 @@
-import requests
+import aiohttp
+import asyncio
+
 API_URL = "https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound"
 LINK_URL = "https://pubchem.ncbi.nlm.nih.gov/compound"
 
@@ -11,38 +13,46 @@ class compound:
     image_url = ""
     url = ""
 
-    def __init__(self, name):
-        resp = requests.get(f"{API_URL}/name/{name}/cids/json").json()
-        if "Fault" in resp:
-            return
-        self.cid = int(resp["IdentifierList"]["CID"][0])
+    def __init__(self):
+        return
+    
+    async def create(self, name):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"{API_URL}/name/{name}/cids/json") as r:
+                if r.status == 200:
+                    js = await r.json()
+                    self.cid = int(js["IdentifierList"]["CID"][0])
 
-        properties = ["Title","MolecularWeight","MolecularFormula"]
-        resp = requests.get(f"{API_URL}/cid/{self.cid}/property/{",".join(properties)}/json").json()
-        if "Fault" in resp:
-            return
-        data = resp["PropertyTable"]["Properties"][0]
-        self.title = data["Title"]
-        self.molecular_weight = float(data["MolecularWeight"])
-        self.molecular_formula = data["MolecularFormula"]
+            properties = ["Title","MolecularWeight","MolecularFormula"]
+            async with session.get(f"{API_URL}/cid/{self.cid}/property/{",".join(properties)}/json") as r:
+                if r.status == 200:
+                    js = await r.json()
+                    data = js["PropertyTable"]["Properties"][0]
+                    self.title = data["Title"]
+                    self.molecular_weight = float(data["MolecularWeight"])
+                    self.molecular_formula = data["MolecularFormula"]
+            async with session.get(f"{API_URL}/cid/{self.cid}/description/json") as r:
+                if r.status == 200:
+                    js = await r.json()
+                    if len(js["InformationList"]["Information"]) > 1:
+                        self.description = js["InformationList"]["Information"][1]["Description"]      
+            self.image_url = f"{API_URL}/cid/{self.cid}/png"
+            self.url = f"{LINK_URL}/{self.cid}"
 
-        resp = requests.get(f"{API_URL}/cid/{self.cid}/description/json").json()
-        if "Fault" in resp:
-            return
-        if len(resp["InformationList"]["Information"]) > 1:
-            self.description = resp["InformationList"]["Information"][1]["Description"]      
-
-        self.image_url = f"{API_URL}/cid/{self.cid}/png"
-        self.url = f"{LINK_URL}/{self.cid}"
-
-#TEST API
-if __name__ == "__main__":
-    compound_data = compound("advil")
+async def main():
+    compound_data = compound()
+    await compound_data.create("advil")
     print(compound_data.cid)
     print(compound_data.title)
     print(compound_data.molecular_weight)
     print(compound_data.molecular_formula)
     print(compound_data.image_url)
     print(compound_data.url)
+
+#TEST API
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
 
     
